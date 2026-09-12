1339d0329b27d31326b71689774d1e239ea29f20
Terje Moe <terjemoe54@hotmail.com>
2026-09-12 22:51:25 +0200

    Initial Commit

---
+++ SaldoPilot/SaldoPilotApp.swift
@@ -0,0 +1,17 @@
+//
+//  SaldoPilotApp.swift
+//  SaldoPilot
+//
+//  Created by Terje Moe on 12/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct SaldoPilotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
